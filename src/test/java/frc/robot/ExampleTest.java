4559ef30a81bce7e38e2182a34f714ae0ba12fec
Zack W <Zack@theWeinbergs.com>
2022-02-08 16:03:54 -0800

    rebuilt with 2022 logic
    used vscode popup to rebuild project with 2022 build logic as recommended by ingi

---
+++ src/test/java/frc/robot/ExampleTest.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+   @Test 
+
+    public void onePlusOneIsTwo(){
+        assertEquals(2, 1+1);
+    }
+}  
